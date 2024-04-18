dce3370f8b1698b7e4c0df7980ba2e7acd973da9
GLetiwa <graceletiwa@gmail.com>
2024-04-18 20:57:25 +0300

    typed annotated function

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type annotated function"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """sums up a floated list"""
+    return sum(input_list)
