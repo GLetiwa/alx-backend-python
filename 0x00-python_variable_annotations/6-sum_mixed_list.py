ddee094bc33fa286875dbf45dfa67de47d7eb242
GLetiwa <graceletiwa@gmail.com>
2024-04-18 21:08:25 +0300

    typed annotated function

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """returns sum of a mixed list(ints & floats)"""
+    return sum(mxd_lst)
