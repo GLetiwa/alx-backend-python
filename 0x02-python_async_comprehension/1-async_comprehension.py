ab31a7956780e553023567251876818e38e1df24
GLetiwa <graceletiwa@gmail.com>
2024-04-23 11:40:27 +0300

    async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""async comprehension coroutine"""
+
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    collect 10 random numbers
+    using async comprehension over
+    async generator
+    """
+    result = [i async for i in async_generator()]
+    return result
